9e384daa193e461819cd4c94c7191957c7b22950
Administrator <Administrator@DESKTOP-A94VGO0>
2019-12-03 16:30:07 +0800

    1

--- DataStructuresAndAlgorithm/Program.cs
+++ DataStructuresAndAlgorithm/Program.cs
@@ -6,7 +6,20 @@ namespace DataStructuresAndAlgorithm
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int Cock, Hen, Chick;
+            for (Cock = 0; Cock <= 20; Cock++)
+            {
+                for (Hen = 0; Hen <= 33; Hen++)
+                {
+                    Chick = 100 - Cock - Hen; 
+                    if (3 * Hen + 5 * Cock + Chick / 3.0 == 100)
+                        Console.WriteLine("公鸡{0}只,母鸡{1}只,小鸡{2}只\n", Cock, Hen, Chick);
+                }
+                count++;
+            }
+            
+            Console.ReadKey();
+
         }
     }
 }
